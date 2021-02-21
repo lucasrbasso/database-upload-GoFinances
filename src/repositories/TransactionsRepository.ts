109b0c3013196e41f56f4f405fb3b8ed99ab3ce2
Lucas Basso <lucasrbasso@users.noreply.github.com>
2021-02-20 23:41:57 -0300

    Function of sum all values

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -3,16 +3,42 @@ import { EntityRepository, Repository } from 'typeorm';
 import Transaction from '../models/Transaction';
 
 interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
+    income: number;
+    outcome: number;
+    total: number;
 }
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  public async getBalance(): Promise<Balance> {
-    // TODO
-  }
+    public async getBalance(): Promise<Balance> {
+        const transactions = await this.find();
+
+        const balance = transactions.reduce(
+            (accumulator: Balance, transaction: Transaction) => {
+                switch (transaction.type) {
+                    case 'income':
+                        accumulator.income += transaction.value;
+                        break;
+                    case 'outcome':
+                        accumulator.outcome += transaction.value;
+                        break;
+                    default:
+                        break;
+                }
+
+                accumulator.total = accumulator.income - accumulator.outcome;
+
+                return accumulator;
+            },
+            {
+                income: 0,
+                outcome: 0,
+                total: 0,
+            },
+        );
+
+        return balance;
+    }
 }
 
 export default TransactionsRepository;
